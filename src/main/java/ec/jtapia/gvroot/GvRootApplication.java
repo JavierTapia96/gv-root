cabfdc766bf3bfd0ca7eebbbf623f286ad345063
Javier Guillermo Tapia Benitez <jtapia@ec.krugercorp.com>
2022-04-23 23:25:16 -0500

    Init project

---
+++ src/main/java/ec/jtapia/gvroot/GvRootApplication.java
@@ -0,0 +1,13 @@
+package ec.jtapia.gvroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GvRootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GvRootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ec/jtapia/gvroot/GvRootApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.jtapia.gvroot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GvRootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
